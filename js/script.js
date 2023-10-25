78d1614f55dd1ebe9ec17bdb2522a76c293e149c
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-10-25 15:44:31 +0200

    Fix: syntax,  i/o   and   instead of

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 const ShoppingList = ["mela", "pera", "latte", "formaggio", "cioccolato"];
+console.log(ShoppingList);
 let ListString = "";
 let i = 0;
 // inizializzazione
 
-while( i > ShoppingList.length) {
+while( i < ShoppingList.length) {
     currentItem = ShoppingList[i];
-    ListString = `<li>${currentItem}</li>`;
+    console.log(currentItem);
+    ListString += `<li>${currentItem}</li>`;
     i++;
 }
 console.log(ListString);
