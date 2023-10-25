f37963cd7e41fc93aae08c9f2563ef5c6e894acf
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-10-25 16:24:25 +0200

    Add: do...while statement i/o while statement

--- js/script.js
+++ js/script.js
@@ -1,14 +1,33 @@
+
+// inizializzazione
+
 const ShoppingList = ["mela", "pera", "latte", "formaggio", "cioccolato"];
 console.log(ShoppingList);
 let ListString = "";
 let i = 0;
-// inizializzazione
+// /inizializzazione
 
-while( i < ShoppingList.length) {
+
+
+// codice con do
+
+do {
     currentItem = ShoppingList[i];
     console.log(currentItem);
     ListString += `<li>${currentItem}</li>`;
     i++;
-}
+} while (i< ShoppingList.length); 
+
 console.log(ListString);
 document.querySelector("ul").innerHTML = ListString;
+
+//codice con while: 
+
+// while( i < ShoppingList.length) {
+//     currentItem = ShoppingList[i];
+//     console.log(currentItem);
+//     ListString += `<li>${currentItem}</li>`;
+//     i++;
+// }
+// console.log(ListString);
+// document.querySelector("ul").innerHTML = ListString;
